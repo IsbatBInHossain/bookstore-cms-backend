1486916d65d35b55f43f167a45ad7efda7d7e176
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-04-26 20:37:14 +0600

    [SET UP] Prisma and [ADD] dockerignore

---
+++ prisma/migrations/20250426133601_initial_migration/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "User_name_email_created_at_updated_at_idx" ON "User"("name", "email", "created_at", "updated_at");
